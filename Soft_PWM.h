37794d4df6acbaecdbbddb3d2b54ad899242f042
Bùi Vạn Văn <134626451+buivanvan@users.noreply.github.com>
2026-04-16 20:14:45 +0700

    Update Soft_PWM.h

--- Soft_PWM.h
+++ Soft_PWM.h
@@ -23,28 +23,41 @@
  * GitHub: https://github.com/buivanvan
  
  * @date    2026
- * @version 1.0
+ * @version 1.1
  */
 
 #ifndef __SOFT_PWM_H
 #define __SOFT_PWM_H
 
 /* ================= INCLUDE ================= */
-/* ========== Chon dong STM32 ban dang dung ===========*/
 #include "stm32f103xb.h"
 
 #define SOFT_PWM_MAX_CH  8
 #define HSE_CLK 8000000
+
+#define Channel0 0
+#define Channel1 1
+#define Channel2 2
+#define Channel3 3
+#define Channel4 4
+#define Channel5 5
+#define Channel6 6
+#define Channel7 7
+
+#define PWM_MODE1 1
+#define PWM_MODE2 2
 
 typedef struct
 {
     GPIO_TypeDef *port;
     uint16_t pin;
     uint16_t duty;      // 0 -> period
+		uint8_t mode;
 } SoftPWM_Channel;
 
 void SoftPWM_Init(uint16_t period);
-uint8_t SoftPWM_AddChannel(GPIO_TypeDef *port, uint16_t pin);
-void SoftPWM_SetDuty(uint8_t ch, uint16_t duty);
+void SoftPWM_AddChannel(GPIO_TypeDef *port, uint16_t pin);
+void SoftPWM_SetDuty(uint8_t channel, uint16_t duty);
+void SoftPWM_ChangeMode(uint8_t channel, uint8_t mode);
 
 #endif
